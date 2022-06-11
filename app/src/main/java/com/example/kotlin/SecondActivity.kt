59a5813a4cb02e88f082fcaee244b281b86d48d5
Abbosbek <vip.real.aa@gmail.com>
2022-06-11 16:24:18 +0500

    Serializable

--- app/src/main/java/com/example/kotlin/SecondActivity.kt
+++ app/src/main/java/com/example/kotlin/SecondActivity.kt
@@ -1,9 +1,12 @@
 package com.example.kotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
+import com.example.kotlin.modle.User
 
 class SecondActivity : AppCompatActivity() {
     val TAG = SecondActivity::class.java.toString()
@@ -12,7 +15,22 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
         initViews()
+        backFinish()
     }
+    fun backFinish(){
+        var b_exit = findViewById<Button>(R.id.b_exit)
+        b_exit.setOnClickListener{
+            var user = User(age = 25, name = "Abbosbek")
+            openMainActivity(user)
+        }
+    }
+    fun openMainActivity(user:User){
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra("user",user)
+
+        startActivity(intent)
+    }
+
     fun initViews(){
         val id_second = findViewById<TextView>(R.id.id_second)
 
---
+++ app/src/main/java/com/example/kotlin/modle/Member.kt
@@ -0,0 +1,7 @@
+package com.example.kotlin.modle
+
+    import java.io.Serializable
+
+class Member(var xabar: String): Serializable {
+
+}
