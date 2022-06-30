737a412d25681696899ad4b794936890ae6bb059
Abbosbek <vip.real.aa@gmail.com>
2022-06-30 14:25:21 +0500

    GridView

---
+++ app/src/main/java/com/example/kotlin/GridAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.kotlin
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.kotlin.modle.Items
+import com.google.api.Context
+
+class GridAdapter(var context: Context, var arrayList: ArrayList<Items>) :BaseAdapter {
+
+
+    override fun getCount(): Int {
+        return arrayList.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return arrayList.get(position)
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var view: View = View.inflate(context, R.layout.row_item,null)
+
+        var icon: ImageView = view.findViewById(R.id.image_view)
+        var name: TextView = view.findViewById(R.id.text_view)
+
+        var items: Items = arrayList.get(position)
+
+        icon.setImageResource(items.icons!!)
+        name.text = items.name
+
+        return view
+    }
+
+
+}
--- app/src/main/java/com/example/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/MainActivity.kt
@@ -1,23 +1,51 @@
 package com.example.kotlin
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
+import android.widget.AdapterView
+import android.widget.GridView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.kotlin.modle.Member
-import com.example.kotlin.modle.User
+import com.example.kotlin.modle.Items
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
 
+    private var gridView: GridView ? = null
+    private var arrayList: ArrayList<Items> ? = null
+    private var gridAdapter:GridAdapter ? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        gridView = findViewById(R.id.grid_view)
+        arrayList = ArrayList()
+        arrayList = setDataList()
+        gridAdapter = GridAdapter(applicationContext, arrayList!!)
+        gridView?.adapter = gridAdapter
+        gridView?.onItemClickListener = this
+    }
+    private fun setDataList(): ArrayList<Items>{
+        var arrayList: ArrayList<Items> = ArrayList()
 
+        arrayList.add(Items(R.drawable.one, "One"))
+        arrayList.add(Items(R.drawable.two, "Two"))
+        arrayList.add(Items(R.drawable.three, "Three"))
+        arrayList.add(Items(R.drawable.four, "Four"))
+        arrayList.add(Items(R.drawable.five, "Five"))
+        arrayList.add(Items(R.drawable.six, "Six"))
+        arrayList.add(Items(R.drawable.seven, "Seven"))
+        arrayList.add(Items(R.drawable.eight, "Eight"))
+        arrayList.add(Items(R.drawable.nine, "Nine"))
+        arrayList.add(Items(R.drawable.ten, "Ten"))
 
+        return arrayList
+    }
+
+    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+        var items:Items = arrayList!!.get(position)
+        Toast.makeText(applicationContext, items.name, Toast.LENGTH_LONG).show()
     }
 
 }
