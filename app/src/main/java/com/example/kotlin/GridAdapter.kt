d487da429db144bea9a1fca74ba025299859e628
Abbosbek <vip.real.aa@gmail.com>
2022-06-30 15:48:23 +0500

    GridView

--- app/src/main/java/com/example/kotlin/GridAdapter.kt
+++ app/src/main/java/com/example/kotlin/GridAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.kotlin
 
+import android.annotation.SuppressLint
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
@@ -8,8 +9,7 @@ import android.widget.TextView
 import com.example.kotlin.modle.Items
 import com.google.api.Context
 
-class GridAdapter(var context: Context, var arrayList: ArrayList<Items>) :BaseAdapter {
-
+class GridAdapter(val context: android.content.Context, var arrayList: ArrayList<Items>) : BaseAdapter() {
 
     override fun getCount(): Int {
         return arrayList.size
@@ -23,8 +23,10 @@ class GridAdapter(var context: Context, var arrayList: ArrayList<Items>) :BaseAd
         return position.toLong()
     }
 
+    @SuppressLint("ViewHolder")
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        var view: View = View.inflate(context, R.layout.row_item,null)
+
+        var view: View = View.inflate(context, R.layout.row_item, null)
 
         var icon: ImageView = view.findViewById(R.id.image_view)
         var name: TextView = view.findViewById(R.id.text_view)
